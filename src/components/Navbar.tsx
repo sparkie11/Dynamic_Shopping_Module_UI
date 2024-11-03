77c2b644d42dabd2be69bfced3fb80daf7105e6e
sparkie11 <gautamwise.s@gmail.com>
2024-11-03 18:33:20 +0530

    n

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+export  function Navbar() {
+  return (
+    <NavbarBs className="bg-white shadow-sm mb-3">
+        <Container>
+            <Nav className="me-auto">
+                <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                <Nav.Link href="/about">About</Nav.Link>
+                <Nav.Link href="/store">Store</Nav.Link>
+            </Nav>
+            <Button className=" rounded-circle " style={{width:'3rem', height: "3rem"}} variant="outline-primary">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z"/></svg>
+            </Button>
+        </Container>
+    </NavbarBs>
+  )
+}
