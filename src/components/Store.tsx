b5b7969cb34255fc92fcaf7763ed02ac9045c18a
sparkie11 <gautamwise.s@gmail.com>
2024-11-03 19:50:41 +0530

    n

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -1,3 +1,25 @@
+import storeItems from "../data/items.json";
+import { Col, Row } from "react-bootstrap";
+
 export function Store(){
-    return <h1> store </h1>
+    return (
+
+        <>
+    <h1> store </h1>
+
+    <Row>
+        {storeItems.map(item =>  (
+        <Col>
+        {JSON.stringify(item)}
+        </Col>
+
+))}
+    </Row>
+
+    
+
+    
+    </>
+    )
+
 }
