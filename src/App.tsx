1885d745bdead5f2f95acf2b332ad05ecf889d54
sparkie11 <gautamwise.s@gmail.com>
2024-11-03 18:33:13 +0530

    n

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import './App.css'
+import {Routes, Route} from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
+import { Store } from "./components/Store";
+import { Navbar } from "./components/Navbar";
 
 function App() {
 
   return (
     <>
-    <h1>hiihi</h1>
+    <Container className="mb-4">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/store" element={<Store />} />
+      </Routes>
+    </Container>
 
     </>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "./index.css";
 import App from './App.tsx'
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  createBrowserRouter,
+  RouterProvider,BrowserRouter
+} from "react-router-dom";
+
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </StrictMode>,
 )
