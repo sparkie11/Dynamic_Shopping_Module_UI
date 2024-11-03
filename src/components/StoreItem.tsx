3132816ae98abcd02a224e3140906ea4d0354cc4
sparkie11 <gautamwise.s@gmail.com>
2024-11-04 00:17:10 +0530

    n

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap"
+import { Button, Card } from "react-bootstrap"
 import { formatCurrency } from "../utilities/formatCurrency"
 
 type StoreItemProps = {
@@ -9,12 +9,19 @@ type StoreItemProps = {
 }
 
 export  function StoreItem({id, name, price, imgUrl} : StoreItemProps) {
+    const quantity= 2;
   return (
-    <Card>
+    <Card className="h-100">
         <Card.Img variant="top" src={imgUrl} height={"200px"} style={{objectFit:'cover'}} />
         <Card.Body>
             <Card.Title>{name}</Card.Title>
             <Card.Text >{formatCurrency(price)}</Card.Text>
+            <div className="mt-auto">
+                {quantity === 0 ?( <Button className="w-100">Add to cart</Button> ) : <div className="d-flex align-items-center flex-column" style={{gap:'.5rem'}}>
+                    <div className="d-flex align-items-center justify-content-center" style={{gap:'.5rem'}}><Button>-</Button><div > <span className="fs-3">{quantity}</span> in cart</div> <Button>+</Button></div><Button variant="danger" size="sm">Remove</Button>
+                </div>}
+
+            </div>
         </Card.Body>
         {/* <Card.Footer>
             <small className="text-muted">ID: {id}</small>
---
+++ src/context/ShoppingCartContext.tsx
@@ -0,0 +1,8 @@
+import { createContext, useContext } from "react";
+
+
+const ShoppingCartContext = createContext({})
+
+ export function useShoppingCart(){
+    return useContext(ShoppingCartContext)
+ }
